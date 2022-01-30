e36bedf2772c6deca820aef3cef6c771ee041256
Michael Billington <michael.billington@gmail.com>
2022-01-30 16:07:04 +1100

    add simple audio

--- demo.s
+++ demo.s
@@ -44,10 +44,11 @@ reset:
 	lda #0
 	sta $2000 ; disable NMI
 	sta $2001 ; disable rendering
-	sta $4015 ; disable APU sound
-	sta $4010 ; disable DMC IRQ
-	lda #$40
-	sta $4017 ; disable APU IRQ
+;
+;	sta $4015 ; disable APU sound
+;	sta $4010 ; disable DMC IRQ
+;	lda #$40
+;	sta $4017 ; disable APU IRQ
 	cld       ; disable decimal mode
 	ldx #$FF
 	txs       ; initialize stack
@@ -90,6 +91,26 @@ reset:
 	sta $2000
 	jmp main
 
+; Based on https://wiki.nesdev.org/w/index.php/APU_basics
+apu_init:         ; Init $4000-4013
+        ldy #$13
+@loop:  lda @apu_startup_values, Y
+        sta $4000, Y
+        dey
+        bpl @loop
+        ; We have to skip over $4014 (OAMDMA)
+        lda #$0f
+        sta $4015
+        lda #$40
+        sta $4017
+        rts
+@apu_startup_values:
+        .byte $30,$08,$00,$00
+        .byte $30,$08,$00,$00
+        .byte $80,$00,$00,$00
+        .byte $30,$00,$00,$00
+        .byte $00,$00,$00,$00
+
 ;
 ; nmi routine
 ;
@@ -103,6 +124,12 @@ scroll_x:       .res 1 ; x scroll position
 scroll_y:       .res 1 ; y scroll position
 scroll_nmt:     .res 1 ; nametable select (0-3 = $2000,$2400,$2800,$2C00)
 temp:           .res 1 ; temporary variable
+ball_x: .res 1
+ball_y: .res 1
+y_frame: .res 1       ; frame for Y-position
+anim_frame: .res 1    ; ball animation frame
+ball_dir: .res 1
+mute_count: .res 1
 
 .segment "BSS"
 nmt_update: .res 256 ; nametable update entry buffer for PPU update
@@ -232,12 +259,6 @@ start_palette: ; background last, foreground second
 .byte $0F,$1B,$2B,$3B ; sp2 not used
 .byte $0F,$12,$22,$32 ; sp3 not used
 
-.segment "ZEROPAGE"
-ball_x: .res 1
-ball_y: .res 1
-y_frame: .res 1       ; frame for Y-position
-anim_frame: .res 1    ; ball animation frame
-ball_dir: .res 0
 
 .segment "CODE"
 main:
@@ -258,9 +279,11 @@ main:
   lda #0
   sta anim_frame
   sta ball_dir
+  sta mute_count
   jsr setup_oam
 	jsr update_ball
 	jsr ppu_update
+  jsr apu_init
 	; main loop
 @draw:
 	; draw everything and finish the frame
@@ -399,19 +422,28 @@ setup_background:
 
 ball_physics:
   jsr ball_physics_x
-  ; set Y
-  inc y_frame
-  ldy y_frame
-  lda ball_loc_y, Y
-  sta ball_y
-  ; set animation frame
-
-  lda ball_x
-; slow mode
-  ror
-  and #%01111111
-  sta anim_frame
+  jsr ball_physics_y
+  jsr ball_physics_spin
+  jsr ball_bounce_mute
   rts
+
+ball_bounce_mute:         ; mute sound eventually
+  dec mute_count
+  bne @end
+  jsr apu_init
+@end:
+  rts
+
+
+  dec mute_count
+  beq @mute
+  rts
+@mute:
+  lda #%00110000
+  sta $400c
+  rts
+  rts
+
 
 ; lookup table for y-coordinates over two bounces
 ball_loc_y: .byte $96, $93, $90, $8d, $8a, $87, $84, $81, $7e, $7b, $78, $75, $72, $6f, $6c, $69, $66, $63, $60, $5e, $5b, $58, $55, $53, $50, $4e, $4b, $49, $46, $44, $42, $3f, $3d, $3b, $39, $37, $35, $33, $31, $2f, $2d, $2c, $2a, $29, $27, $26, $24, $23, $22, $21, $20, $1f, $1e, $1d, $1c, $1b, $1b, $1a, $1a, $19, $19, $19, $19, $19, $19, $19, $19, $19, $19, $1a, $1a, $1b, $1b, $1c, $1c, $1d, $1e, $1f, $20, $21, $22, $24, $25, $26, $28, $29, $2b, $2d, $2e, $30, $32, $34, $36, $38, $3a, $3c, $3e, $40, $43, $45, $48, $4a, $4c, $4f, $52, $54, $57, $5a, $5c, $5f, $62, $65, $68, $6a, $6d, $70, $73, $76, $79, $7c, $7f, $82, $85, $89, $8c, $8f, $92, $95, $95, $92, $8f, $8c, $89, $85, $82, $7f, $7c, $79, $76, $73, $70, $6d, $6a, $68, $65, $62, $5f, $5c, $5a, $57, $54, $52, $4f, $4c, $4a, $48, $45, $43, $40, $3e, $3c, $3a, $38, $36, $34, $32, $30, $2e, $2d, $2b, $29, $28, $26, $25, $24, $22, $21, $20, $1f, $1e, $1d, $1c, $1c, $1b, $1b, $1a, $1a, $19, $19, $19, $19, $19, $19, $19, $19, $19, $19, $1a, $1a, $1b, $1b, $1c, $1d, $1e, $1f, $20, $21, $22, $23, $24, $26, $27, $29, $2a, $2c, $2d, $2f, $31, $33, $35, $37, $39, $3b, $3d, $3f, $42, $44, $46, $49, $4b, $4e, $50, $53, $55, $58, $5b, $5e, $60, $63, $66, $69, $6c, $6f, $72, $75, $78, $7b, $7e, $81, $84, $87, $8a, $8d, $90, $93, $96
@@ -431,6 +463,7 @@ ball_physics_x:
   bne @ball_move_end
   lda #BALL_LEFT          ; change direction
   sta ball_dir
+  jsr bounce_noise
   jmp @ball_move_end
   @ball_move_left:
   dec ball_x              ; move left
@@ -439,5 +472,35 @@ ball_physics_x:
   bne @ball_move_end
   lda #BALL_RIGHT         ; change direction
   sta ball_dir
+  jsr bounce_noise
   @ball_move_end:
+  rts
+
+ball_physics_y:           ; set Y from lookup table
+  inc y_frame
+  ldy y_frame
+  lda ball_loc_y, Y
+  sta ball_y
+  cmp #$95
+  bcc @end
+  jsr bounce_noise
+@end:
+  rts
+
+ball_physics_spin:       ; set animation frame
+  lda ball_x
+  ; slow mode
+  ror
+  and #%01111111
+  sta anim_frame
+  rts
+
+bounce_noise:
+  jsr apu_init
+  lda #%00000101        ; some raw noise
+  sta $400E
+  lda #%00110001
+  sta $400C
+  lda #5
+  sta mute_count
   rts
